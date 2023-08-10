ae4b17b10094017b49319748f9fa85a69793f1f7
dishant <dishantpanchani122@gmail.com>
2023-08-10 18:50:18 +0530

    socket url

--- pages/data.tsx
+++ pages/data.tsx
@@ -17,7 +17,7 @@ import Popover from '@mui/material/Popover';
 
 
      // Create a Socket.IO instance and connect to the server
-     const socket = io('http://localhost:4001');
+     const socket = io(`http://${window.location.hostname}:3000`);
 const user = router.query.data
      console.log("router",router.query.data)
      // Listen for messages from the server
